e1318960d1d5e866556b189d6b9ce49fa90302c4
Turgut Kaan Turan <tkaanturan@gmail.com>
2022-03-23 22:05:43 +0300

    Add project files.

---
+++ WinFormApp/Form1.cs
@@ -0,0 +1,22 @@
+﻿namespace WinFormApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("Butonun üzerine gelindi");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Butona Tıklandı");
+        }
+
+       
+    }
+}
---
+++ app2/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace app2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.txtIsim = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGoster = new System.Windows.Forms.Button();
+            this.lstOgrenciler = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(24, 77);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(242, 29);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // txtIsim
+            // 
+            this.txtIsim.Location = new System.Drawing.Point(24, 44);
+            this.txtIsim.Name = "txtIsim";
+            this.txtIsim.Size = new System.Drawing.Size(242, 27);
+            this.txtIsim.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Öğrenci Adı";
+            // 
+            // btnGoster
+            // 
+            this.btnGoster.Location = new System.Drawing.Point(24, 111);
+            this.btnGoster.Name = "btnGoster";
+            this.btnGoster.Size = new System.Drawing.Size(242, 29);
+            this.btnGoster.TabIndex = 4;
+            this.btnGoster.Text = "Eklenen Kayıtları Göster";
+            this.btnGoster.UseVisualStyleBackColor = true;
+            this.btnGoster.Click += new System.EventHandler(this.btnGoster_Click);
+            // 
+            // lstOgrenciler
+            // 
+            this.lstOgrenciler.FormattingEnabled = true;
+            this.lstOgrenciler.ItemHeight = 20;
+            this.lstOgrenciler.Location = new System.Drawing.Point(27, 158);
+            this.lstOgrenciler.Name = "lstOgrenciler";
+            this.lstOgrenciler.Size = new System.Drawing.Size(239, 344);
+            this.lstOgrenciler.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 561);
+            this.Controls.Add(this.lstOgrenciler);
+            this.Controls.Add(this.btnGoster);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIsim);
+            this.Controls.Add(this.btnEkle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnEkle;
+        private TextBox txtIsim;
+        private Label label1;
+        private Button btnGoster;
+        private ListBox lstOgrenciler;
+    }
+}
